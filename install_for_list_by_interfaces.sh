af51d74817e3ebb03790f79bbd6c2375d0eb64c7
VikneshSougoumar <sougoumar@intechinfo.fr>
2016-06-02 12:17:12 +0200

    Delete install_for_list_by_interfaces.sh

--- install_for_list_by_interfaces.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install nbtscan
-sudo apt-get install python-tk python-imaging-tk python3-tk
-sudo aptitude install python3-setuptools
-sudo easy_install3 pip
-sudo aptitude install python3-dev
-sudo pip3 install netifaces
