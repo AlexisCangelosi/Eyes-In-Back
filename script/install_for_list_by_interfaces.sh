2bcd29a922c9b213ecb5d6a0eb0a1ee336d01f81
VikneshSougoumar <sougoumar@intechinfo.fr>
2016-06-02 12:18:43 +0200

    Add files via upload
    installations des paquets pour le listing des ordinateurs présents sur le réseau avec une interface

---
+++ script/install_for_list_by_interfaces.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get install nbtscan
+sudo apt-get install python-tk python-imaging-tk python3-tk
+sudo aptitude install python3-setuptools
+sudo easy_install3 pip
+sudo aptitude install python3-dev
+sudo pip3 install netifaces
